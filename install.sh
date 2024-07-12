f7c4f1818c68fbe30b2039523c74d16800d19c5a
SathvikPN <sathvikapn@gmail.com>
2024-07-13 02:00:24 +0530

    adds installation script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Installation directory
+INSTALL_DIR="/usr/local/bin"
+CONFIG_FILE="$HOME/.delconfig"
+
+# Clone the repository
+git clone https://github.com/SathvikPN/del.git /tmp/del
+
+# Move the main script to the installation directory
+sudo mv /tmp/del/del.sh "$INSTALL_DIR/del"
+sudo chmod +x "$INSTALL_DIR/del"
+
+# Move the auto purge script to the installation directory
+sudo mv /tmp/del/purge.sh "$INSTALL_DIR/purge"
+sudo chmod +x "$INSTALL_DIR/purge"
+
+# Copy the example configuration file to the user's home directory if not present
+if [ ! -f "$CONFIG_FILE" ]; then
+  cp /tmp/del/.delconfig "$CONFIG_FILE"
+fi
+
+
+
+# Clean up
+rm -rf /tmp/del
+
+echo "Installation completed. You can now use 'del' command."
