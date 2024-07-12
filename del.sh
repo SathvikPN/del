a262743a4ac2927cc7e617808d7b46a5ee7c5713
SathvikPN <sathvikapn@gmail.com>
2024-07-12 23:53:29 +0530

    implements move-to-trash

--- del.sh
+++ del.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DEL_VERSION='0.0.1'
+TRASH_DIR="$HOME/.trash_directory"
+VERBOSE=false
+
 if [ $# -eq 0 ]; then
   echo "Welcome to 'del'
   - a trashing utility for your linux bash terminal
@@ -22,25 +26,46 @@ Options:
 EOF
 }
 
-# parse command-line options
-# =~ bash pattern matching operator, true if $1 matches ^-(string that starts with -)
-while [[ "$1" =~ ^- ]]; do 
-  case $1 in
-    -h | --help)
-      show_help
-      exit 0
-      ;; # ends case
-    -v | --verbose)
-      echo "option for verbose output"
-      ;;
-    --version)
-      echo "del version 0.0.0"  # Assuming the next argument is the author's name
-      shift  # Move past the argument value
-      ;;
-    *)
-      echo "Unknown option: $1"
-      exit 1
-      ;;
-  esac
-  shift  # Move to the next argument $1 <-- $2 (now $1 points to 2nd positional arg)
-done
+move_to_trash() {
+  local file=$1
+  if [ -e $file ]; then 
+    local dest="$TRASH_DIR/$(basename "$file")_$(date +%s)"
+    mv "$file" "$dest"
+    [ "$VERBOSE" == true ] && echo "trashed $file to $dest"
+  else
+    echo "file not found: $file"
+  fi
+}
+
+
+main() {
+  # parse command-line options -------------------------------------------------------
+  # =~ bash pattern matching operator, true if $1 matches ^-(string that starts with -)
+  while [[ "$1" =~ ^- ]]; do 
+    case $1 in
+      -h | --help)
+        show_help
+        exit 0
+        ;; # ends case
+      -v | --verbose)
+        VERBOSE=true
+        shift
+        ;;
+      --version)
+        echo "del version 0.0.0"  # Assuming the next argument is the author's name
+        shift  # Move past the argument value
+        ;;
+      *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift  # Move to the next argument $1 <-- $2 (now $1 points to 2nd positional arg)
+  done  
+
+  mkdir -p $TRASH_DIR # create trash_dir if not exist
+
+  for file in "$@"; do 
+    move_to_trash "$file"
+  done 
+}
