6f0b3a6d4f73129ce4e71f056f1e3d5eb3c9822e
SathvikPN <sathvikapn@gmail.com>
2024-07-15 22:27:11 +0530

    adds config for del with edit option

--- del.sh
+++ del.sh
@@ -3,8 +3,27 @@
 VERSION=1.0.0
 AUTHOR=sathvikpn
 
-# default configs
+# default configs 
 VERBOSE=0
+CONFIG_FILE="${HOME}/.delconfig"
+DEFAULT_TRASH_DIR="${HOME}/.local/share/Trash/files"
+DEFAULT_AUTO_PURGE_DAYS=30
+CRON_JOB_ID="del_cleanup"
+
+create_default_config() {
+cat <<EOF > "$CONFIG_FILE"
+TRASH_DIR="${DEFAULT_TRASH_DIR}"
+AUTO_PURGE_DAYS=${DEFAULT_AUTO_PURGE_DAYS}
+EOF
+}
+
+del_init(){
+    if [[ ! -f "$CONFIG_FILE" ]]; then
+        create_default_config
+    fi
+    source "$CONFIG_FILE"
+    mkdir -p $TRASH_DIR
+}
 
 show_help() {
 cat << EOF
@@ -24,7 +43,21 @@ show_version() {
     echo "author: $AUTHOR"
 }
 
+edit_config() {
+    ${EDITOR:-vi} "$CONFIG_FILE"
+    # Reload configuration after editing
+    source "$CONFIG_FILE"
+}
+
+
+
+
+
+
+
 # script starts here ----------------------------------------------------------
+
+del_init
 
 # parse args
 case "$1" in
@@ -41,6 +74,7 @@ case "$1" in
         exit 0
         ;;
     --config)
+        edit_config
         exit 0
         ;;
 esac
